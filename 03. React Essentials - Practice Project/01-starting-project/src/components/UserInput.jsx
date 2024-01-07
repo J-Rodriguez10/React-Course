c96c6be86c4cc5ae0ab0bc7a0b500f0b62894ff8
Jesus Rodriguez <jesusrod789@gmail.com>
2024-01-07 11:07:18 -0800

    add project 03

---
+++ 03. React Essentials - Practice Project/01-starting-project/src/components/UserInput.jsx
@@ -0,0 +1,50 @@
+export default function UserInput({onChange, userInput}) {
+
+
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(ev) =>
+              onChange("initialInvestment", ev.target.value)
+            }
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(ev) => onChange("annualInvestment", ev.target.value)}
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(ev) => onChange("expectedReturn", ev.target.value)}
+          />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(ev) => onChange("duration", ev.target.value)}
+          />
+        </p>
+      </div>
+    </section>
+  );
+}
